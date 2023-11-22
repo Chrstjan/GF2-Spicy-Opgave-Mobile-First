9621bfc4e58e40470c2d2dce47189f60fd463c42
Chrstjan <ps5christian@hotmail.com>
2023-11-22 10:43:42 +0100

    Added validation for the phonenumber
    need to rework the dialog styling so that the error messages fits the screen

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -3,16 +3,6 @@ const modal = document.getElementById("myDialog");
 const openBtn = document.getElementById("openModal");
 const closeBtn = document.getElementById("closeModal");
 
-//Form
-const form = document.getElementById("myForm");
-const fName = document.getElementById("fName");
-const nameRegExp = /^[a-zA-Z\s]+$/;
-const phoneNumber = document.getElementById("telNumber");
-const email = document.getElementById("Email");
-const emailRegExp =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+){1,2}$/;
-const errors = document.getElementById("error");
-const requirements = document.getElementsByClassName("formRequiredment");
 //Opening and closing the modal
 openBtn.addEventListener("click", () => {
   modal.style.display = "block";
@@ -22,10 +12,24 @@ closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
+//Form
+const form = document.getElementById("myForm");
+const fName = document.getElementById("fName");
+const nameRegExp = /^[a-zA-Z\s]+$/;
+const phoneNumber = document.getElementById("telNumber");
+const numberRegExp = /^[0-9+]{8,11}$/;
+const email = document.getElementById("Email");
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+){1,2}$/;
+const errors = document.getElementById("error");
+const requirements = document.getElementsByClassName("formRequiredment");
+
 //JS Validating the form
 form.addEventListener("submit", (e) => {
   const maxNameLength = 30;
+  const maxNumberLength = 11;
   let errorMessages = [];
+
   //Validating name
   const trimmedName = fName.value.trim();
   const nameIsValid =
@@ -34,8 +38,10 @@ form.addEventListener("submit", (e) => {
     nameRegExp.test(trimmedName);
   if (!nameIsValid) {
     errorMessages.push("Name is required and must be bewteen 4 and 30 letters");
+    requirements[0].style.color = "red";
   } else {
     requirements[0].style.color = "green";
+    console.log("Name is valid");
   }
 
   if (trimmedName.length > maxNameLength) {
@@ -43,16 +49,32 @@ form.addEventListener("submit", (e) => {
     fName.value = trimmedName.slice(0, maxNameLength);
   }
 
+  //Validating Phone number
+  const numberIsValid = phoneNumber.value.length <= maxNumberLength && numberRegExp.test(phoneNumber.value);
+  if (!numberIsValid) {
+    errorMessages.push("A valid phone number is required");
+    requirements[1].style.color = "red";
+  }
+  else {
+    requirements[1].style.color = "green";
+    console.log("Number is valid");
+  }
   //Validating Email
   const emailIsValid = email.value.length > 0 && emailRegExp.test(email.value);
   if (!emailIsValid) {
     errorMessages.push("A valid email is required");
+    requirements[2].style.color = "red";
   } else {
     requirements[2].style.color = "green";
+    console.log("Email is valid");
   }
 
   if (errorMessages.length > 0) {
     e.preventDefault();
+  }
+
+  if (nameIsValid && numberIsValid && emailIsValid) {
+    console.log("Form udfyldt korrekt")
   }
 });
 
